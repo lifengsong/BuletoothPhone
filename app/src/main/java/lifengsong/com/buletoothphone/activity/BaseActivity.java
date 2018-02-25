c19f6905a81cd63e07e72aa39cf40720b9155db2
lilei <1058509673@qq.com>
2018-02-25 20:29:55 +0800

    base config

---
+++ app/src/main/java/lifengsong/com/buletoothphone/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package lifengsong.com.buletoothphone.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lilei on 2018/2/25.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
